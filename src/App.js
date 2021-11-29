7871af5d065bbaff597dc82f54ffb5d3c7863042
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-11-29 10:11:55 -0500

    auto setup send email

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [subject, setSubject] = useState('');
+  const [emailDest, setEmailDest] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubjectChange = ({target}) => {
+    setSubject(target.value);
+  }
+  const handleEmailChange = ({target}) => {
+    setEmailDest(target.value);
+  }
+  const handleBodyChange = ({target}) => {
+    setBody(target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.open(`mailto:${emailDest}?subject=${subject}&body=${body}`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +38,39 @@ function App() {
           Learn React
         </a>
       </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='title'>Title:</label>
+        <input
+          type='text'
+          id='subject'
+          value={subject}
+          onChange={handleSubjectChange}
+          placeholder={'enter subject'}
+          required
+        />
+        <br/>
+        <label htmlFor='emailDest'>Email Destination:</label>
+        <input
+          type='text'
+          id='emailDest'
+          value={emailDest}
+          onChange={handleEmailChange}
+          placeholder={'enter email destination'}
+          required
+        />
+        <br/>
+        <label htmlFor='body'>Email Body:</label>
+        <textarea
+          id='body'
+          rows="4"
+          cols="50"
+          value={body}
+          onChange={handleBodyChange}
+          required
+        />
+        <br/>
+        <input type='submit' value='Submit' />
+      </form>
     </div>
   );
 }
